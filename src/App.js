da0e21f05830fea83cd8ec9619746c8361e1b9d1
chlwl <chlwlgus89@naver.com>
2020-05-31 20:27:11 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import MyComponent from './MyComponent';
 import './App.css';
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from './ScrollBox';
+import IterationSample from "./IterationSample";
 
-function App() {
-    const name = '리액트';
-    const style = {
-      backgroundColor: 'black',
-      color: 'aqua',
-      fontSize:'48px',
-      fontWeight:'bold',
-      padding:16
-    };
-    return (
-        <Fragment>
-            <div style={style}>{name}</div>
-            <div className="react">{name}</div>
-            <MyComponent name={'React'} favoriteNumber={1}>리액트</MyComponent>
-            <Counter/>
-            <Say/>
-            <EventPractice />
-        </Fragment>
-    );
+// function App() {
+class App extends Component {
+    // const name = '리액트';
+    // const style = {
+    //     backgroundColor: 'black',
+    //     color          : 'aqua',
+    //     fontSize       : '48px',
+    //     fontWeight     : 'bold',
+    //     padding        : 16
+    // };
+    render() {
+        return (
+            <Fragment>
+                {/*<div style={style}>{name}</div>*/}
+                {/*<div className="react">{name}</div>*/}
+                <MyComponent name={'React'}
+                             favoriteNumber={1}
+                             ref={(ref) => {this.myComponent = ref}}
+                >리액트</MyComponent>
+                <Counter/>
+                <Say/>
+                <EventPractice/>
+                <ValidationSample/>
+                <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+                <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+                <IterationSample/>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
